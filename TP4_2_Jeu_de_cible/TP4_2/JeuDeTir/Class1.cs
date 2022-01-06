edecaaca1f591114097cbf31c7b8262533dff187
adkhey746 <97223037+adkhey746@users.noreply.github.com>
2022-01-06 10:27:04 +0100

    Add files via upload

---
+++ TP4_2_Jeu_de_cible/TP4_2/JeuDeTir/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JeuDeTir
+{
+    public class Constantes
+    {
+        // constantes pour la fenetre SFML
+        public const int TAILLE_FENETRE = 800;
+        public const int TAILLE_PETITE_CIBLE = 20;
+        public const int TAILLE_MOYENNE_CIBLE = 40;
+        public const int TAILLE_GRANDE_CIBLE = 60;
+        public const int POINTS_PETITE_CIBLE = 60;
+        public const int POINTS_MOYENNE_CIBLE = 40;
+        public const int POINTS_GRANDE_CIBLE = 20;
+        public const int NB_ESSAIS_MAX = 3;
+        // constantes pour la fenetre console
+        public const int CONSOLE_NB_COL = 40;
+        public const int CONSOLE_NB_LIGNE = 20;
+    }
+}
